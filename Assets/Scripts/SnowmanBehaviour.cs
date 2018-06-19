6e8fd8f62b85076f800ab91feb52eb94375b6b31
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-06-19 11:46:22 -0500

    snowman can ascend

---
+++ Assets/Scripts/SnowmanBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowmanBehaviour : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 force = Vector3.up;
+        Rigidbody rb3d = GetComponent<Rigidbody>();
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb3d.AddForce(force * 20);
+        }
+	}
+}
